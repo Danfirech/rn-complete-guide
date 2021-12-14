1e8468ea5296bf050004b162e580ed56f4ae342c
Dan Possehl <danielbpossehl@gmail.com>
2021-12-14 09:54:37 -0700

    Text prints from input box to below space

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
+  const [enteredGoal, SetEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    SetEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((CurrentGoals) => [...courseGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goals" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Course Goals"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -26,5 +50,14 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     height: 30,
+  },
+
+  listItem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    width: "80%",
+    marginVertical: 10,
   },
 });
